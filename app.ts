24955d1edcf60cfca8104fc4791681031802777f
tohsaka888 <tohsaka888@Tohsaka.localdomain>
2021-08-01 08:21:01 +0800

    新增登陆/登陆状态接口

--- app.ts
+++ app.ts
@@ -1,18 +1,23 @@
 import { Application, Router } from "https://deno.land/x/oak@v8.0.0/mod.ts";
-import { searchArticle, insertArticle } from "./mongoConnection.ts";
+import {
+  searchArticle,
+  insertArticle,
+  searchUser,
+  User,
+} from "./mongoConnection.ts";
 
 const app = new Application();
 const router = new Router();
 
 // 设置响应头
 const responseHeader = new Headers({
-  'content-type': 'application/json;charset=UTF-8',
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': '*',
-  'Access-Control-Allow-Headers': '*',
-})
+  "content-type": "application/json;charset=UTF-8",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+  "Access-Control-Allow-Headers": "*",
+});
 
-console.log("博客服务已经在8080端口启用");
+console.log("博客服务已经在8085端口启用");
 
 // 路由匹配
 router.get("/", (ctx) => {
@@ -25,12 +30,12 @@ router.post("/addArticle", async (ctx) => {
     const requestBody = await ctx.request.body().value;
     if (requestBody) {
       const id = await insertArticle(requestBody);
-      ctx.response.headers = responseHeader
+      ctx.response.headers = responseHeader;
       ctx.response.status = 200;
       ctx.response.body = { _id: id, message: "插入成功" };
     }
   } catch (error) {
-    const errorMsg:string = error.name + error.message 
+    const errorMsg: string = error.name + error.message;
     ctx.response.status = 300;
     ctx.response.body = { error: errorMsg, message: "插入失败" };
   }
@@ -40,6 +45,59 @@ router.get("/getArticles", async (ctx) => {
   ctx.response.status = 200;
   ctx.response.headers = responseHeader;
   ctx.response.body = await searchArticle();
+});
+
+//处理登陆请求
+router.post("/login", async (ctx) => {
+  try {
+    let message: string;
+    ctx.response.status = 200;
+    const user: User = await ctx.request.body().value;
+    if (user) {
+      const dbStatus: number | undefined = await searchUser(
+        user.username,
+        user.password
+      );
+      switch (dbStatus) {
+        case 0:
+          message = "登陆成功";
+          break;
+        case 1:
+          message = "密码错误";
+          break;
+        case 2:
+          message = "用户不存在";
+          break;
+        default:
+          message = "";
+          break;
+      }
+      ctx.response.headers = responseHeader;
+      ctx.response.status = 200;
+      ctx.cookies.set("loginUser", user.username);
+      ctx.response.body = {
+        message: message,
+        code: 200,
+        cookie: ctx.cookies.get("loginUser"),
+      };
+    }
+  } catch (error) {
+    const errorMsg: string = error.name + error.message;
+    ctx.response.status = 300;
+    ctx.response.headers = responseHeader;
+    ctx.response.body = { error: errorMsg, message: "登陆失败", code: 300 };
+  }
+});
+
+//获取登陆状态
+router.get("/login/status", (ctx) => {
+  if (ctx.cookies.get("loginUser")) {
+    ctx.response.body = {
+      login: true,
+      message: `欢迎${ctx.cookies.get("loginUser")}`,
+      code: 200,
+    };
+  }
 });
 
 app.use(router.routes());
--- mongoConnection.ts
+++ mongoConnection.ts
@@ -17,16 +17,47 @@ type Article = {
   intro: string;
 };
 
+type User = {
+  username: string;
+  password: string;
+};
+
 // 选择数据库
 const db: Database = client.database("blog");
 
 // 选择数据库中的集合
 const articleCollection: Collection<Article> =
   db.collection<Article>("articles");
+const userCollection: Collection<User> = db.collection<User>("userList");
 
 // 查询操作(所有)
 const searchArticle = async () => {
   return await articleCollection.find({}).toArray();
+};
+
+const searchUser = async (
+  username: string,
+  password: string
+): Promise<number | undefined> => {
+  const user = await userCollection.find({}).toArray();
+  let dbStatus: number | undefined = -1;
+  user.map((item: User, index: number) => {
+    if (item.username === username && item.password === password) {
+      // 0登陆成功
+      dbStatus = 0;
+    } else {
+      if (item.username === username) {
+        // 1密码错误
+        dbStatus = 1;
+        return 0
+      }
+      if (index === user.length - 1) {
+        // 2没有注册过
+        dbStatus = 2;
+      }
+    }
+  });
+  return dbStatus;
 };
 
 // 更新操作
@@ -42,5 +73,5 @@ const insertArticle = async (
   const id: Bson.Document = await articleCollection.insertOne(article);
   return id;
 };
-export type { Article };
-export { insertArticle, searchArticle, updateOneArticle };
+export type { Article, User };
+export { insertArticle, searchArticle, updateOneArticle, searchUser };
